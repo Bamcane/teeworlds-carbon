62a377ba79793c8ffd99afbcb3e1b73d3c56d7f2
Bamcane <diandaokui@qq.com>
2025-08-09 14:12:05 +0800

    Fix minor client problems and  append carbon msg.

--- src/engine/message.h
+++ src/engine/message.h
@@ -3,20 +3,29 @@
 #ifndef ENGINE_MESSAGE_H
 #define ENGINE_MESSAGE_H
 
+#include <base/uuid.h>
 #include <engine/shared/packer.h>
 
 class CMsgPacker : public CPacker
 {
 public:
-	CMsgPacker(int Type, bool System = false)
+	CMsgPacker(int Type, bool System = false, bool Carbon = false)
 	{
+
 		Reset();
 		if(Type < 0 || Type > 0x3FFFFFFF)
 		{
 			m_Error = true;
 			return;
 		}
-		AddInt((Type << 1) | (System ? 1 : 0));
+		if(Carbon)
+		{
+			AddInt(System ? 1 : 0);
+			AddRaw(&UUID_CARBON_NAMESPACE, sizeof(UUID_CARBON_NAMESPACE));
+			AddInt(Type);
+		}
+		else
+			AddInt((Type << 1) | (System ? 1 : 0));
 	}
 };
 
@@ -24,8 +33,10 @@ class CMsgUnpacker : public CUnpacker
 {
 	int m_Type;
 	bool m_System;
-
+	const Uuid *m_pNamespace;
 public:
+	CMsgUnpacker() = default;
+
 	CMsgUnpacker(const void *pData, int Size)
 	{
 		Reset(pData, Size);
@@ -40,10 +51,23 @@ public:
 		}
 		m_System = Msg & 1;
 		m_Type = Msg >> 1;
+		if(m_Type == 0)
+		{
+			m_pNamespace = (const Uuid *) GetRaw(sizeof(*m_pNamespace));
+			if(m_Error)
+			{
+				m_System = false;
+				m_Type = 0;
+				m_pNamespace = nullptr;
+				return;
+			}
+		}
 	}
 
 	int Type() const { return m_Type; }
 	bool System() const { return m_System; }
+	const Uuid *Namespace() const { return m_pNamespace; }
+	void ModifyType(int Type) { m_Type = Type; }
 };
 
 #endif
